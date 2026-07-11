7a65112a9b38c2acbb445345d5d7df1db91c4788
Juan Vilas <108366198+juan-vilas@users.noreply.github.com>
2026-07-11 19:04:27 +0200

    init

---
+++ src/main/java/com/learning/kafka/wikimedia/Main.java
@@ -0,0 +1,7 @@
+package com.learning.kafka.wikimedia;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("kafka-wikimedia-changes starting...");
+    }
+}
